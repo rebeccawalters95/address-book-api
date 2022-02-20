a86984a4bba607fd63a3d7f8e78c01e1fc7f3195
rebeccawalters95 <42864940+rebeccawalters95@users.noreply.github.com>
2022-02-20 16:25:59 +0000

    updated serialiser to include get put post, need to add user auth!!

--- address_api/addresses/views.py
+++ address_api/addresses/views.py
@@ -1,10 +1,28 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-
 from .serializers import AddressSerializer
 from .models import Address
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import permissions
+from rest_framework import renderers
+
 class AddressViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+
+    Additionally we also provide an extra `highlight` action.
+    """
     queryset = Address.objects.all()
     serializer_class = AddressSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+     #TODO add in owner 
+    #def perform_create(self, serializer):
+     #   serializer.save(owner=self.request.user)
